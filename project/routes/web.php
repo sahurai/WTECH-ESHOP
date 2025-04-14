b8ea79b30c8e78442d0ca9e3f62eb632f3a16c01
Ann1771 <87091158+Ann1771@users.noreply.github.com>
2025-04-14 17:09:36 +0200

    change recommendations and add simple test

--- project/routes/web.php
+++ project/routes/web.php
@@ -25,3 +25,42 @@ Route::get('/', function () {
 
     return view('category', compact('books', 'isAdmin'));
 });
+
+
+Route::get('/product-demo', function () {
+    $book = [
+        'id' => 1,
+        'title' => 'Hunting Adeline',
+        'author' => 'H.D. Carlton',
+        'image' => 'book-cover-1.png',
+        'price' => '18,03',
+        'discount' => 18,
+        'description' => 'A dark romance thriller with mystery and suspense.',
+        'pages' => 300,
+        'genre' => 'Mystery',
+        'category' => 'Classic',
+        'year' => 2020,
+        'language' => 'English',
+        'format' => 'Hardcover',
+        'publisher' => 'XYZ Publishing',
+        'isbn' => '978-3-16-148410-0',
+        'edition' => '1st',
+        'dimensions' => '15x21 cm',
+        'weight' => 500
+    ];
+
+    $recommended = [
+        $book, // you can repeat the same book for simplicity
+        $book,
+        $book,
+        $book,
+        $book,
+        $book
+    ];
+
+    return view('product-page', [
+        'book' => $book,
+        'recommendations' => $recommended,
+        'isAdmin' => true // or true if you want to test admin mode
+    ]);
+});
