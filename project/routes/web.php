7f3b165e9f7c435863c61f21dfea4ada350d8e51
sahurai <108337296+sahurai@users.noreply.github.com>
2025-04-22 15:54:29 +0200

    Update web.php

--- project/routes/web.php
+++ project/routes/web.php
@@ -6,17 +6,6 @@ use App\Http\Controllers\BookController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\OrderController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group.
-|
-*/
-
 // --------------------------------------------------------------------------
 // Authentication Routes
 // --------------------------------------------------------------------------
